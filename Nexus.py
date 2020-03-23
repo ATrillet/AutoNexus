6e7ce228ff084154be9c283f7699be7beb830a1a
Andrew Trillet <andrew.trillet@gmail.com>
2020-03-22 21:31:29 -0400

    optimize and added healing

--- Nexus.py
+++ Nexus.py
@@ -18,15 +18,18 @@ def on_press_loop(key):
     if key == keyboard.Key.alt_l:
         return False
 
-def auto_nexus(hpx, hpy):
-    im1 = ImageGrab.grab(bbox=(x,y,x+1,y+1))  # x1, y1, x2, y2
+def auto_nexus(hpx, hpy, x_diff):
+    im1 = ImageGrab.grab(bbox=(hpx,hpy,hpx+x_diff,hpy+1))  # x1, y1, x2, y2
+    xnexus = x_diff/hp_percent
+    xheal = x_diff/2
     rgb_im = im1.convert('RGB')
-    a,b,c = rgb_im.getpixel((0, 0))
-    if a==84 and b==84 and c==84:
-        board.press('f')
+    a1,b1,c1 = rgb_im.getpixel((xnexus, 0))
+    a2,b2,c2 = rgb_im.getpixel((xheal, 0))
+    if a1==84 and b1==84 and c1==84:
         board.press('r')
-        print("dying")
         time.sleep(4)
+    elif a2==84 and b2==84 and c2==84:
+        board.press('f')
 
 
 
@@ -45,54 +48,32 @@ def find_hp_bar(hpr,hpg,hpb):
     arrayy = []
 
     for y in range(0, midh):
-        pixsub = []
         for x in range(midw, w):
             r,g,b = rgb_im.getpixel((x, y))
-            if r==224 and g==52 and b==52:
+            if r==hpr and g==hpg and b==hpb:
                 arrayx.append(x)
                 arrayy.append(y)
 
     pixarray = []
+
     x1 = arrayx[0]
     x2 = arrayx[-1]
-    diff = x2-x1
-    offsetx = int(diff/hp_percent)
-    x3 = x1 + offsetx
-
+    xdiff = x2-x1
 
     y1 = arrayy[0]
-    y2 = arrayy[-1]
-    diff = y2-y1
-    offsety = 1
-    y3 = y1 + offsety
 
-    r,g,b = rgb_im.getpixel((x3, y3))
+    return x1, y1, xdiff
+    
 
-
-
-    for y in range(y1-1,y3-1):
-        pixsub = []
-        for x in range(x1, x3):
-            pixsub.append(rgb_im.getpixel((x, y)))
-        pixarray.append(pixsub)
-
-    new_array = np.array(pixarray, dtype=np.uint8)
-    new_image = Image.fromarray(new_array)
-    new_image.save('new_pic.png')
-    return x3,y3
-
-x,y = find_hp_bar(224,52,52)
-im1 = ImageGrab.grab(bbox=(x,y,x+1,y+1))
-rgb_im = im1.convert('RGB')
-r,g,b = rgb_im.getpixel((0, 0))
-# print(r,g,b,sep=',')
+xs, y, xdiff = find_hp_bar(224,52,52)
 
 test = True
 print("starting")
 while test:
+    auto_nexus(xs,y, xdiff)
     with keyboard.Listener(on_press=on_press_end) as listener2:
         time.sleep(.01)
         if not listener2.running:
             print('end')
             test = False
-    auto_nexus(x,y)
+    
