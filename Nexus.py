c79338ad89c903969b19320c32ed0370a4e72ddc
Andrew Trillet <andrew.trillet@gmail.com>
2020-03-26 06:30:54 -0400

    minor updates

--- Nexus.py
+++ Nexus.py
@@ -7,7 +7,8 @@ from pynput import keyboard
 import time
 
 #realm tick rate 0.2s
-hp_percent = 3
+hp_percent = 23
+heal_percent = 50
 board = Controller()
 
 def on_press_end(key):
@@ -20,13 +21,14 @@ def on_press_loop(key):
 
 def auto_nexus(hpx, hpy, x_diff):
     im1 = ImageGrab.grab(bbox=(hpx,hpy,hpx+x_diff,hpy+1))  # x1, y1, x2, y2
-    xnexus = x_diff/hp_percent
-    xheal = x_diff/2
+    xnexus = int((hp_percent/100)*x_diff)
+    xheal = int((heal_percent/100)*x_diff)
     rgb_im = im1.convert('RGB')
     a1,b1,c1 = rgb_im.getpixel((xnexus, 0))
     a2,b2,c2 = rgb_im.getpixel((xheal, 0))
     if a1==84 and b1==84 and c1==84:
         board.press('r')
+        print("dying")
         time.sleep(4)
     elif a2==84 and b2==84 and c2==84:
         board.press('f')
